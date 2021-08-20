7c4e96bdc6c00e63b176cc1cd3d357462da3ed2d
stevieknox <stefanotart@gmail.com>
2021-08-19 22:14:30 -0400

    5.1

--- application.py
+++ application.py
@@ -4,11 +4,12 @@ import plotly.express as px
 import requests
 import streamlit as st
 import application_function 
+import matplotlib.pyplot as plt
 st.set_page_config(layout='wide')
 scatter_column, setting_column = st.columns((4,1))
 
 user_input = setting_column.text_input("Enter the first 3 digits of your Postal Code(eg. H3G", 'Postal Code')
 scatter_column.table(application_function.post_look_up(user_input))
-scatter_column.plotly_chart(px.bar(data_frame=application_function.YoY(user_input)))
+scatter_column.line_chart(data=application_function.YoY(user_input))
 
 
