b71abc1743e8b03561460cb468c4b236d4d849e3
stevieknox <stefanotart@gmail.com>
2021-08-19 22:27:31 -0400

    5.1

--- application.py
+++ application.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import streamlit as st
 import application_function 
-st.set_page_config(layout='wide')
 scatter_column, setting_column = st.columns((4,1))
 
-user_input = setting_column.text_input("Enter the first 3 digits of your Postal Code(eg. H3G")
+user_input = setting_column.text_input("Enter the first 3 digits of your Postal Code(eg. H3G)")
 scatter_column.table(application_function.post_look_up(user_input))
 scatter_column.line_chart(data=application_function.YoY(user_input))
 
