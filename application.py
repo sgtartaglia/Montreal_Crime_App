d3b2bf05da95fb18aeec5c17e64d3f77ae45fa67
stevieknox <stefanotart@gmail.com>
2021-08-19 22:56:46 -0400

    5.1

--- application.py
+++ application.py
@@ -4,7 +4,11 @@ import application_function
 scatter_column, setting_column = st.columns((4,1))
 
 user_input = setting_column.text_input('Enter first 3 characters of postal')
-scatter_column.table(application_function.post_look_up(user_input))
-scatter_column.line_chart(data=application_function.YoY(user_input))
+if user_input is not None:
+    scatter_column.table(application_function.post_look_up(user_input))
+    scatter_column.line_chart(data=application_function.YoY(user_input))
+else:
+    scatter_column.header("please enter a postal code")
+
 
 
