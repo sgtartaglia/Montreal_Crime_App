2bb3783b085075d17ccef714b64e41c2203698db
stevieknox <stefanotart@gmail.com>
2021-08-19 22:34:06 -0400

    5.1

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import application_function
 scatter_column, setting_column = st.columns((4,1))
 
 setting_column.text('Enter the first 3 digits of your Postal Code(eg.H3G)')
-user_input = setting_column.text_input(question)
+user_input = setting_column.text_input()
 scatter_column.table(application_function.post_look_up(user_input))
 scatter_column.line_chart(data=application_function.YoY(user_input))
 
