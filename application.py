c583a0bf6e734bdf396924c0b1aa5c4a8eda5ff3
stevieknox <stefanotart@gmail.com>
2021-08-25 17:53:34 -0400

    08/25

--- application.py
+++ application.py
@@ -15,7 +15,7 @@ selection_button = setting_column.radio(label='look up postal',options=('look up
 if selection_button == 'look up postal code':
     user_input = setting_column.text_input(label='Enter first 3 characters of postal',key='user_choice')
     if user_input != '':
-        scatter_column.dataframe(application_function.post_look_up(user_input[:3]))
+        scatter_column.table(application_function.post_look_up(user_input[:3]))
         scatter_column.line_chart(data=application_function.YoY(user_input[:3]))
         setting_column.text(application_function.get_neighbourhood(user_input[:3]))
     else:
